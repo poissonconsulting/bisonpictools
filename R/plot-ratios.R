1fe492fec69c2e5a7875654fd204b85c8129677a
andreakortello <andrea@poissonconsulting.ca>
2023-07-31 15:15:57 -0230

    add license headers to R files

--- R/plot-ratios.R
+++ R/plot-ratios.R
@@ -1,3 +1,17 @@
+#Copyright 2023 Province of Alberta
+
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+
+#http://www.apache.org/licenses/LICENSE-2.0
+
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+
 #' Plot Ratios of Ronald Wood Bison camera events
 #' 
 #' Generates bubble plot of ratio of the sex-age classes supplied to `numerator` and `denominator`. The ratio is given as numerator`:(`denominator` + `numerator`)) to avoid infinite values when the numerator is ≥ 1 and the denominator = 0. Each point represents an event. The size of the point represents the total herd size, and the colour of the point represents the value of the ratio.
--- R/seasons.R
+++ R/seasons.R
@@ -1,3 +1,17 @@
+#Copyright 2023 Province of Alberta
+
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+
+#http://www.apache.org/licenses/LICENSE-2.0
+
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+
 #' Generate Seasons Data
 #' 
 #' Generates a table of the dates of different seasons relevant to Ronald Wood Bison movement patterns: calving, summer/fall, and winter. Start and end dates of each season are based in the year 1972.
