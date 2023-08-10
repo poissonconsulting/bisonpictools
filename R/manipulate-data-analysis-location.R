fcd0b1d866f771323ef77d4a3ef51799c18e4db6
Nicole Hill <nicole@poissonconsulting.ca>
2023-08-10 16:07:12 -0700

    Add skeletons of manipulation functions for analysis

---
+++ R/manipulate-data-analysis-location.R
@@ -0,0 +1,12 @@
+#' Prepare Location Data for Analysis
+#'
+#' @param location_data a tibble of templated location data
+#'
+#' @return a tibble of manipulated location data
+#' @export
+#'
+#' @examples
+#' manipulate_data_analysis_location(location_data())
+manipulate_data_analysis_location <- function(location_data) {
+  location_data
+}
