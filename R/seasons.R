acd1db40456e8a31a342dce7636e935daab12925
Nicole Hill <nicole@poissonconsulting.ca>
2023-08-15 16:05:29 -0700

    Extend winter season daytes into 1973

--- R/seasons.R
+++ R/seasons.R
@@ -14,7 +14,7 @@
 
 #' Generate Seasons Data
 #' 
-#' Generates a table of the dates of different seasons relevant to Ronald Wood Bison movement patterns: calving, summer/fall, and winter. Start and end dates of each season are based in the year 1972.
+#' Generates a table of the dates of different seasons relevant to Ronald Wood Bison movement patterns: calving, summer/fall, and winter. Start and end dates of each season are based in the study year 1972-1973.
 #'
 #' @return A tibble
 #' @export
@@ -24,16 +24,11 @@
 seasons <- function() {
   dplyr::tribble(
           ~season,           ~start_dayte,            ~end_dayte,
-        "Calving",  "1972-04-01 00:00:00", "1972-06-30 00:00:00",
-    "Summer/Fall",  "1972-07-01 00:00:00", "1972-11-30 00:00:00",
-         "Winter",  "1972-12-01 00:00:00", "1972-12-31 00:00:00",
-         "Winter",  "1972-01-01 00:00:00", "1972-03-31 00:00:00",
+        "Calving",  "1972-04-01 00:00:00", "1972-06-30 23:59:59",
+    "Summer/Fall",  "1972-07-01 00:00:00", "1972-11-30 23:59:59",
+         "Winter",  "1972-12-01 00:00:00", "1973-03-31 23:59:59"
   ) |> 
   dplyr::mutate(
-    dplyr::across( 
-      c("start_dayte", "end_dayte"),
-      \(x) dttr2::dtt_date_time(x) |> 
-        dttr2::dtt_dayte_time()
-    )
+    dplyr::across(c("start_dayte", "end_dayte"), \(x) dttr2::dtt_date_time(x)),
   )
 }
