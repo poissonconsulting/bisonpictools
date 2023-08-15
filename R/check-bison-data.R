8abd96be6fe4a297c11260c956881f5f32b9cf48
Ayla Pearson <ayla@poissonconsulting.ca>
2023-08-14 17:27:12 -0700

    adding in function to do checks in more dynamic manner

--- R/check-bison-data.R
+++ R/check-bison-data.R
@@ -3,31 +3,86 @@
 #' Confirms the columns types match the template, the values follow the allowed
 #' ranges of the template and tables can be joined appropriately. 
 #'
-#' @param data A list containing the location and event data 
-#' @param template A list of the templates
+#' @param event A data frame of event data.
+#' @param location A data frame of location data.
+#' @param complete A flag indicating if all data frames need to be supplied.
 #'
 #' @return List of data frames
 #'
 #' @examples
-#' bpt_check_data(
-#'   list(
-#'     Locations = location_data(),  
-#'     Events = event_data()
-#'    )
-#'  )
-bpt_check_data <- function(data, template) {
+bpt_check_data <- function(event = NULL, location = NULL, complete = FALSE) {
+  chk_datas(
+    event = event,
+    location = location,
+    template = bisonpictools::template,
+    complete = complete
+  )
+}
+
+### move this to a package
+chk_datas <- function(..., template, joins = NULL, complete = FALSE) {
+  data <- list(...)
   
+  template_names <- names(template)
+  
+  data[sapply(data, is.null)] <- NULL
+  data_names <- names(data)
+  
+  if (!any(data_names %in% template_names)) {
+    chk::abort_chk(
+      "Table name(s) doesn't match template sheet names"
+    )
+  }
+  
+  # if complete = TRUE then must have all names represented
+  if (complete) {
+    if (!all(template_names %in% data_names)) {
+      stop("All sheets must be present")
+    }
+  }
+  
+  # check individual datasets
   data <- check_column_types(data, template)
   data <- check_template_ranges(data, template)
-  chk_tbl_join(data)
+
+  
+  
+  # if complete = TRUE then check joins
   
   data
 }
 
+events <- event_data()
+# try a bad value
+events[2, 7] <- 1004
+# change column type
+events$f1 <- as.character(events$f1) 
+
+dat <- bpt_check_data(event = events, location = location_data(), complete = TRUE)
+dat <- bpt_check_data(event = events, location = location_data(), complete = FALSE)
+dat1 <- bpt_check_data(location = location_data(), complete = TRUE)
+dat1 <- bpt_check_data(location = location_data(), complete = FALSE)
+dat2 <- bpt_check_data(event = events, complete = TRUE)
+dat2 <- bpt_check_data(event = events, complete = FALSE)
+
+
+dat2
+
+# # prep below
+# bpt_check_data <- function(data) {
+# 
+#   data <- check_column_types(data, template)
+#   data <- check_template_ranges(data, template)
+#   chk_tbl_join(data)
+# 
+#   data
+# }
+
+
 chk_tbl_join <- function(data) {
   location <- data[["Locations"]]
   event <- data[["Events"]]
-  
+
   if (!chk::vld_join(event, location, by = c("location_id"))) {
     chk::abort_chk(
       "Not all `location_id` values in the event table are in the locations
@@ -48,25 +103,25 @@ check_types <- function(data, template) {
   chk::chk_superset(names(data), names(x), x_name = "column names in data")
   extra <- setdiff(names(data), names(x))
   data <- data[c(names(x), extra)]
-  
+
   type_vals <- chktemplate::chkrow_to_type(template[template$name == "chk", ])
-  
+
   integer_cols <- names(data)[which(type_vals == "integer")]
   numeric_cols <- names(data)[which(type_vals == "numeric")]
   char_cols <- names(data)[which(type_vals == "character")]
-  
+
   for (i in integer_cols) {
     data[[i]] <- safe_as_integer(data[[i]], i)
   }
-  
+
   for (i in numeric_cols) {
     data[[i]] <- safe_as_numeric(data[[i]], i)
   }
-  
+
   for (i in char_cols) {
     data[[i]] <- as.character(data[[i]])
   }
-  
+
   data
 }
 
@@ -120,3 +175,4 @@ check_template_ranges <- function(data, template) {
   }
   data
 }
+
