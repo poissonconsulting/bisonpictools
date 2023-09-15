662ac56e068008b968fad4492ba4d7ed00c5028a
Nicole Hill <nicole@poissonconsulting.ca>
2023-09-15 15:47:21 -0700

    Remove unncessary examples

--- R/bpt-check-data.R
+++ R/bpt-check-data.R
@@ -8,6 +8,7 @@
 #' @param complete A flag indicating if all data frames need to be supplied.
 #'
 #' @return List of data frames
+#' @export
 #'
 #' @examples
 #' \dontrun{
@@ -15,11 +16,14 @@
 #' data <- bpt_check_data(
 #'   event = bpt_event_data, location = bpt_location_data, complete = TRUE
 #' )
+#' event_data <- data$event
+#' location_data <- data$location
 #' # To check only a single data set
 #' data_1 <- bpt_check_data(location = bpt_location_data)
 #' data_2 <- bpt_check_data(event = bpt_event_data)
-#' location <- bpt_check_data(location = bpt_location_data, complete = FALSE)
-#' event <- bpt_check_data(event = bpt_event_data, complete = FALSE)
+#' 
+#' # The following should produce an error, as `complete = TRUE` requires that 
+#' both data sets are provided
 #' event <- bpt_check_data(event = bpt_event_data, complete = TRUE)
 #' }
 bpt_check_data <- function(event = NULL, location = NULL, complete = FALSE) {
