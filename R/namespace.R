962463de3ea0c55c52f2bd572ebbefe704076314
Nicole Hill <nicole@poissonconsulting.ca>
2023-10-18 15:30:27 -0700

    Import sf and units packages

--- R/namespace.R
+++ R/namespace.R
@@ -1,9 +1,12 @@
 #' @import chk
+#' @import chktemplate
 #' @import dttr2
 #' @import dplyr
 #' @import forcats
 #' @import ggplot2
 #' @importFrom rlang .data
+#' @import sf
 #' @import stringr
 #' @import tidyr
+#' @import units
 NULL
