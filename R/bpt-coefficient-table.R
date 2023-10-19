039f2afc83faf43056dfe0efe9508cbdf727d787
Nicole Hill <nicole@poissonconsulting.ca>
2023-10-19 16:00:37 -0700

    Generate coefficient table

---
+++ R/bpt-coefficient-table.R
@@ -0,0 +1,22 @@
+#' Extract Coefficient Table
+#' Extracts the coefficient table from the analysis object.
+#'
+#' @param analysis the analysis object from `bpt_analyse()`
+#'
+#' @return a tibble of coefficients with columns: 
+#'  - `term` the name of the term
+#'  - `estimate` the median posterior estimate
+#'  - `lower` the lower 95% credible interval
+#'  - `upper` the upper 95% credible interval
+#'  - `svalue` a measure of directionality with respect to zero
+#'    - an s-value of 4.32 bits is equivalent to a p-value of 0.05
+#' @export
+#'
+#' @examples 
+#' \dontrun{
+#' bpt_coefficient_table(analysis)
+#' }
+bpt_coefficient_table <- function(analysis) {
+  coef <- coef(analysis, simplify = TRUE) 
+  coef
+}
