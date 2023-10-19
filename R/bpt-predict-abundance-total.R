2ec2bf744971da096ae4b7aafa2a0e2832cc0d06
Nicole Hill <nicole@poissonconsulting.ca>
2023-10-19 16:03:05 -0700

    Predict total abundance

---
+++ R/bpt-predict-abundance-total.R
@@ -0,0 +1,23 @@
+#' Predict Total Abundance
+#'
+#' Predicts the total herd abundance for each study year, as the sum of the
+#' predicted abundances for each class.
+#'
+#' @param analysis the analysis object from `bpt_analyse()`
+#'
+#' @return a tibble of total abundances for each study year, including the
+#'   median posterior value (estimate) and lower and upper 95% credible
+#'   intervals
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' bpt_predict_abundance_total(analysis)
+#' }
+
+bpt_predict_abundance_total <- function(analysis) {
+  data <- embr::data_set(analysis)
+  abundance <- embr::predict(analysis, "annual", term = "eAbundance") |> 
+    dplyr::select("annual", "estimate", "lower", "upper", "svalue")
+  abundance
+}
