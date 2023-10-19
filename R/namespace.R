3fae54ca2bf71d0dd2a633e4975bf57cb6a684c8
Nicole Hill <nicole@poissonconsulting.ca>
2023-10-19 16:03:45 -0700

    Remove most imports

--- R/namespace.R
+++ R/namespace.R
@@ -1,12 +1,3 @@
 #' @import chk
-#' @import chktemplate
-#' @import dttr2
-#' @import dplyr
-#' @import forcats
-#' @import ggplot2
 #' @importFrom rlang .data
-#' @import sf
-#' @import stringr
-#' @import tidyr
-#' @import units
 NULL
