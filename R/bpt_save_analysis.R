c68a0312b7d9970f2c4002507a9ae7934685b654
Nicole Hill <nicole@poissonconsulting.ca>
2023-10-19 18:33:43 -0700

    Function to save analysis

---
+++ R/bpt_save_analysis.R
@@ -0,0 +1,16 @@
+#' Save analysis object
+#' 
+#' Saves analysis object to the file location specified by `file`.
+#'
+#' @param analysis analysis object from `bpt_analyse()`
+#' @param file name of the file where the analysis object is saved to
+#'
+#' @export
+#'
+#' @examples 
+#' \dontrun{
+#' bpt_save_analysis(analysis, "output/analysis")
+#' }
+bpt_save_analysis <- function(analysis, file) {
+  base::saveRDS(object = analysis, file = base::paste0(file, ".RDS"))
+}
