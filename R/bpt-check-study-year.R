d5eb5a1bfaae997601d051ccc4af1d5b4b9cf50a
Nicole Hill <nicole@poissonconsulting.ca>
2023-11-15 14:22:22 -0800

    Check join between study years of census data and calf proportion data and the study years of event data

---
+++ R/bpt-check-study-year.R
@@ -0,0 +1,35 @@
+#' Check Study Years
+#' 
+#' Checks that the dates provided in the census and calf proportion data are 
+#' within the study years of the event data.
+#'
+#' @param event_data a tibble of templated event data
+#' @param census_data a tibble of templated census data
+#' @param proportion_calf_data a tibble of templated calf proportion data
+#'
+#' @return Throws an error if the study years do not match
+#' @export
+#'
+#' @examples
+#' bpt_check_study_year(
+#'   bpt_event_data,
+#'   bpt_census_data,
+#'   bpt_proportion_calf_data
+#' )
+bpt_check_study_year <- function(
+  event_data,
+  census_data,
+  proportion_calf_data
+) {
+  sy_event <- bpt_study_years(event_data)
+  sy_census <- bpt_study_years(census_data)
+  sy_prop_calf <- bpt_study_years(proportion_calf_data)
+  
+  if (!chk::vld_subset(sy_census, sy_event)) {
+    chk::abort_chk("Census data must include only dates that are within the study years of the event data.")
+  }
+  
+  if (!chk::vld_subset(sy_prop_calf, sy_event)) {
+    chk::abort_chk("Calf proportion data must include only dates that are within the study years of the event data.")
+  }
+}
---
+++ tests/testthat/test-bpt-check-study-year.R
@@ -0,0 +1,39 @@
+test_that("NULL inputs throw error", {
+  expect_chk_error(
+    bpt_check_study_year(NULL, NULL, NULL),
+    regexp = "Data is not a compatible tibble\\. Ensure columns match the formatting for one of the `event`, `census`, or `proportion_calf` tables in the template\\: see `\\?template`\\."
+  )
+  expect_chk_error(
+    bpt_check_study_year(bpt_event_data, NULL, NULL),
+    regexp = "Data is not a compatible tibble\\. Ensure columns match the formatting for one of the `event`, `census`, or `proportion_calf` tables in the template\\: see `\\?template`\\."
+  )
+  expect_chk_error(
+    bpt_check_study_year(NULL, bpt_census_data, NULL),
+    regexp = "Data is not a compatible tibble\\. Ensure columns match the formatting for one of the `event`, `census`, or `proportion_calf` tables in the template\\: see `\\?template`\\."
+  )
+})
+
+
+test_that("error thrown if study years of census data don't match those of event data", {
+  census_data <- bpt_census_data |> 
+    dplyr::mutate(census_year = c(2030, 2031))
+  expect_chk_error(
+    bpt_check_study_year(bpt_event_data, census_data, bpt_proportion_calf_data),
+    regexp = "Census data must include only dates that are within the study years of the event data."
+  )
+})
+
+test_that("error thrown if study years of census data don't match those of event data", {
+  prop_calf_data <- bpt_proportion_calf_data |> 
+    dplyr::mutate(proportion_calf_year = c(2030, 2031))
+  expect_chk_error(
+    bpt_check_study_year(bpt_event_data, bpt_census_data, prop_calf_data),
+    regexp = "Calf proportion data must include only dates that are within the study years of the event data."
+  )
+})
+
+test_that("passes if the study years of both census and prop calf data match those of event data", {
+  expect_no_error(
+    bpt_check_study_year(bpt_event_data, bpt_census_data, bpt_proportion_calf_data)
+  )
+})
