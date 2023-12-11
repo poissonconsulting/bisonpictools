8d446ab5ef49f6b7e59b28115063f37c849720c0
Nicole Hill <nicole@poissonconsulting.ca>
2023-12-11 14:59:36 -0800

    Remove super arrow and combine tests

--- tests/testthat/test-bpt-analyse.R
+++ tests/testthat/test-bpt-analyse.R
@@ -1,22 +1,18 @@
-test_that("no errors when running analysis", {
-  expect_no_error(
-    analysis <<- bpt_analyse(
-      event_data = bpt_event_data,
-      location_data = bpt_location_data,
-      census_data = bpt_census_data,
-      proportion_calf_data = bpt_proportion_calf_data,
-      nthin = 1L,
-      analysis_mode = "quick"
-    )
+test_that("analysis runs producing an .smb_analysis object with correct thinning rate", {
+  analysis <- bpt_analyse(
+    event_data = bpt_event_data,
+    location_data = bpt_location_data,
+    census_data = bpt_census_data,
+    proportion_calf_data = bpt_proportion_calf_data,
+    nthin = 1L,
+    analysis_mode = "quick"
   )
-})
-
-test_that("analysis object is an .smb_analysis", {
+  
   expect_equal(class(analysis), c("smb_analysis", "mb_analysis"))
-})
-
-test_that("thinning rate is 1", {
   expect_true(analysis$nthin == 1L)
 })
 
 ### TODO: Add tests about specific parameters when model finalized.
+# 1. Does it output the stan errors in debug mode?
+# 2. If we add a seed to the analysis, can test that we get the same parameters
+#    provided that we don't change the data..
