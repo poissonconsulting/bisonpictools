75a63c563ca5a15908b729d2c85a4fa7434119e8
Nicole Hill <nicole@poissonconsulting.ca>
2024-04-02 08:36:32 -0700

    Add expect_snapshot_data

--- R/test-helpers.R
+++ R/test-helpers.R
@@ -26,3 +26,15 @@ expect_snapshot_plot <- function(x, name) {
   path <- save_png(x)
   testthat::expect_snapshot_file(path, paste0(name, ".png"))
 }
+
+save_csv <- function(x) {
+  path <- tempfile(fileext = ".csv")
+  readr::write_csv(x, path)
+  path
+}
+
+expect_snapshot_data <- function(x, name) {
+  testthat::skip_on_os("windows")
+  path <- save_csv(x)
+  testthat::expect_snapshot_file(path, paste0(name, ".csv"))
+}
