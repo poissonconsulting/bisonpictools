b93e8b1a94260ce216b4da79ad0d28b43dd5617d
Nicole Hill <nicole@poissonconsulting.ca>
2024-04-09 08:16:49 -0700

    Lint

--- inst/test-objects/save-test-analysis.R
+++ inst/test-objects/save-test-analysis.R
@@ -3,7 +3,6 @@ if (FALSE) {
   event_data <- bisonpictools::bpt_event_data
   census_data <- bisonpictools::bpt_census_data
   proportion_calf_data <- bisonpictools::bpt_proportion_calf_data
-  
   analysis <- bpt_analyse(
     event_data = event_data,
     location_data = location_data,
@@ -12,9 +11,7 @@ if (FALSE) {
     nthin = 1L,
     analysis_mode = "quick"
   )
-  
   bpt_save_analysis(analysis, "inst/test-objects/analysis")
-  
   glance <- embr::glance(analysis)
   saveRDS(glance, "inst/test-objects/glance.RDS")
 }
