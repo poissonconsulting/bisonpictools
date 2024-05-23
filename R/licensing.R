d7ae2d5091ca2312313cf50a98539b18e8a83b26
Joe Thorley <joe@poissonconsulting.ca>
2024-05-23 07:49:20 -0700

    added `bpt_licensing_md()`.

---
+++ R/licensing.R
@@ -0,0 +1,24 @@
+#' Licensing Markdown
+#' 
+#' A string of markdown code indicating the licensing of the code 
+#' and documentation
+#' @export
+#' @examples
+#' bpt_licensing_md()
+bpt_licensing_md <- function() {
+'## Licensing
+  Copyright 2023 Province of Alberta
+  
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+  
+  http://www.apache.org/licenses/LICENSE-2.0
+  
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+'
+}
---
+++ tests/testthat/test-licensing.R
@@ -0,0 +1,3 @@
+test_that("licensing works", {
+  expect_true(chk::vld_string(bpt_licensing_md()))
+})
