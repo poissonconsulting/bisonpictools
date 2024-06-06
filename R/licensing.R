589adeaa97526761377febe92d2f14124d72a76d
Ayla Pearson <ayla@poissonconsulting.ca>
2024-06-06 09:50:23 -0700

    styler

--- R/licensing.R
+++ R/licensing.R
@@ -1,20 +1,20 @@
 #' Licensing Markdown
-#' 
-#' A string of markdown code indicating the licensing of the code 
+#'
+#' A string of markdown code indicating the licensing of the code
 #' and documentation
 #' @export
 #' @examples
 #' bpt_licensing_md()
 bpt_licensing_md <- function() {
-'## Licensing
+  '## Licensing
   Copyright 2023 Province of Alberta
-  
+
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
   You may obtain a copy of the License at
-  
+
   http://www.apache.org/licenses/LICENSE-2.0
-  
+
   Unless required by applicable law or agreed to in writing, software
   distributed under the License is distributed on an "AS IS" BASIS,
   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
